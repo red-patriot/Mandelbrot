7ff4d9cb678b5bcfef50da5ad04a3b8341c28304
Bryson Tanner <realredpatriot@gmail.com>
2020-02-02 16:25:19 -0700

    Added improved transformation slope tracking

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -1,6 +1,8 @@
 #include "Mandelbrot.h"
+
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 using std::complex;        using std::pair;
 
@@ -11,7 +13,7 @@ Mandelbrot::Mandelbrot(int escape_limit) :
   escape_time_limit(escape_limit),
   gathering_new_limits(false) { }
 
-bool Mandelbrot::init(std::complex<long double> max, std::complex<long double> min,
+bool Mandelbrot::init(std::complex<double> max, std::complex<double> min,
                       int width, int height) {
   /* Initialize necessary subsystems. */
   // Initialize SDL systems and objects
@@ -135,8 +137,8 @@ void Mandelbrot::gather_new_limits() {
   }
 }
 
-void Mandelbrot::set_plot_limits(std::complex<long double> first,
-                                 std::complex<long double> second) {
+void Mandelbrot::set_plot_limits(std::complex<double> first,
+                                 std::complex<double> second) {
   /* Set new plot limits from two points defining the new bounding rectangle.
    * The new points don't have to be ordered, so maxs and mins are determined.
    */
@@ -148,14 +150,14 @@ void Mandelbrot::set_plot_limits(std::complex<long double> first,
 
 void Mandelbrot::reset_plot_resolution() {
   /* Set a new plot resolution and ready the plot for recalculation. */
-  long double new_plot_resolution = determine_resolution();
+  double new_plot_resolution = determine_resolution();
   points.clear();
   state = NeedsToGeneratePoints;
   escape_time_limit *= 2;
   plot_resolution = new_plot_resolution;
 }
 
-long double Mandelbrot::determine_resolution() {
+double Mandelbrot::determine_resolution() {
   /* Determine the plot resolution based on the window and plot dimensions. */
   return std::min((plot_max.real() - plot_min.real())/window_width,
                   (plot_max.imag() - plot_min.imag())/window_height);
@@ -165,10 +167,10 @@ void Mandelbrot::generate_points() {
   /* Generate a vector of points for the calculation. */
   if (state == NeedsToGeneratePoints) {
     std::cout << "start_gp\n";
-    for (long double r = plot_min.real(); r <= plot_max.real(); r += plot_resolution) {
-      for (long double i = plot_min.imag(); i <= plot_max.imag(); i += plot_resolution) {
-        complex<long double> c{r, i};
-        points.push_back(pair<complex<long double>, unsigned int>(c, 0));
+    for (double r = plot_min.real(); r <= plot_max.real(); r += plot_resolution) {
+      for (double i = plot_min.imag(); i <= plot_max.imag(); i += plot_resolution) {
+        complex<double> c{r, i};
+        points.push_back(pair<complex<double>, unsigned int>(c, 0));
       }
     }
     state = NeedsToCalculateEscape;
@@ -188,21 +190,22 @@ void Mandelbrot::calculate_escape_times() {
   }
 }
 
-unsigned int Mandelbrot::iterate_point(const std::complex<long double>& c) {
+unsigned int Mandelbrot::iterate_point(const std::complex<double>& c) {
   /* Iterate over a single point to determine how long it takes to excape. */
-  complex<long double> _c{c.real(), c.imag()};
-  complex<long double> z{0.0, 0.0};
+  complex<double> _c{c.real(), c.imag()};
+  complex<double> z{0.0, 0.0};
   unsigned int count = 1;
 
   // check if the point is within the cardiod or period-2 bulb first
-  long double x{c.real()}, y{c.imag()};
-  long double q = (x-.25)*(x-.25) + y*y;
+  double x{c.real()}, y{c.imag()};
+  double q = (x-.25)*(x-.25) + y*y;
   if ((q*(q + (x-.25))) <= (.25*y*y) || ((x+1)*(x+1) + y*y) <= .0625) {
     return 0;
   }
 
   while (count < escape_time_limit) {
     z = z*z + _c;
+    // check if z has escaped the bounds of the set
     if (z.real()*z.real() + z.imag()*z.imag() >= 4.0){
       break;
     }
@@ -212,7 +215,7 @@ unsigned int Mandelbrot::iterate_point(const std::complex<long double>& c) {
   return count == escape_time_limit ? 0 : count;
 }
 
-void Mandelbrot::render_point(std::pair<std::complex<long double>, unsigned int> point) {
+void Mandelbrot::render_point(std::pair<std::complex<double>, unsigned int> point) {
   /* Render a point on the plot. */
   Color c = calculate_color(point.second);
   SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 255);
@@ -222,10 +225,10 @@ void Mandelbrot::render_point(std::pair<std::complex<long double>, unsigned int>
 
 Mandelbrot::Color Mandelbrot::calculate_color(const unsigned int e) {
   /* Calculate the color of a point based on its escape_time. */
-  return e == 0 ? Color(0,0,0) :
-    Color{static_cast<unsigned short>((e > 200 ? 10*e : 0)%255),
+  return e == 0 ? Color(0,0,0) : Color(175, 175, 175);
+  /*    Color{static_cast<unsigned short>((e > 200 ? 10*e : 0)%255),
           static_cast<unsigned short>((e > 100 ? 14*e : 0)%255),
-          static_cast<unsigned short>(7*e%255)};
+          static_cast<unsigned short>(7*e%255)};*/
 }
 
 void Mandelbrot::alert_new_limits() {
@@ -242,22 +245,26 @@ void Mandelbrot::alert_new_limits() {
   SDL_RenderPresent(renderer);
 }
 
-inline long double Mandelbrot::xtosdl(long double x) {
-  return (window_width * x)/(plot_max.real() - plot_min.real())
+inline double Mandelbrot::xtosdl(double x) {
+  double m = (window_width)/(plot_max.real() - plot_min.real());
+  return m * x
     - (window_width*plot_min.real())/(plot_max.real() - plot_min.real());
 }
 
-inline long double Mandelbrot::ytosdl(long double y){
-  return (window_height * y)/(plot_min.imag() - plot_max.imag())
+inline double Mandelbrot::ytosdl(double y){
+  double m = (window_height)/(plot_min.imag() - plot_max.imag());
+  return m * y
     - (window_height*plot_max.imag())/(plot_min.imag() - plot_max.imag());
 }
 
-inline long double Mandelbrot::sdltox(long double sdlx) {
-  return ((plot_max.real() - plot_min.real())*sdlx)/(window_width)
+inline double Mandelbrot::sdltox(double sdlx) {
+  double m = (plot_max.real() - plot_min.real())/(window_width);
+  return m * sdlx
     + plot_min.real();
 }
 
-inline long double Mandelbrot::sdltoy(long double sdly) {
-  return ((plot_min.imag() - plot_max.imag())*sdly)/(window_height)
+inline double Mandelbrot::sdltoy(double sdly) {
+  double m = (plot_min.imag() - plot_max.imag())/(window_height);
+  return m * sdly
     + plot_max.imag();
 }
